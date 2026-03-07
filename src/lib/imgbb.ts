2e8fd2b57c382667a0ef33b20733168068e0a657
DrawSenger <test@example.com>
2026-03-07 21:11:32 +0300

    Добавлен API Key для Cloudinary

--- src/lib/imgbb.ts
+++ src/lib/imgbb.ts
@@ -1,11 +1,13 @@
 const CLOUD_NAME = 'dvy0f64be';
 const UPLOAD_PRESET = 'ml_default';
+const API_KEY = '576761668982569';
 
 export const uploadImageToImgBB = async (file: File): Promise<string> => {
   try {
     const formData = new FormData();
     formData.append('file', file);
     formData.append('upload_preset', UPLOAD_PRESET);
+    formData.append('api_key', API_KEY);
 
     console.log('Uploading file to Cloudinary:', file.name, file.size, file.type);
 
@@ -17,13 +19,14 @@ export const uploadImageToImgBB = async (file: File): Promise<string> => {
     const data = await response.json();
     
     if (!response.ok) {
-      throw new Error(data.error?.message || 'Ошибка загрузки в Cloudinary');
+      console.error('Детали ошибки Cloudinary:', data);
+      throw new Error(data.error?.message || 'Ошибка загрузки');
     }
 
     console.log('Успешно загружено:', data.secure_url);
     return data.secure_url;
   } catch (error: any) {
-    console.error('Ошибка при загрузке файла:', error);
+    console.error('Ошибка:', error);
     throw new Error(`File upload failed: ${error.message}`);
   }
 };
